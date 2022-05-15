24d234b7903844ddc7a0f0607a2e325fa96dfab0
ileanachamorro <ic.chamorro07@gmail.com>
2022-05-14 23:58:34 -0300

    Editar Post en Blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import View
+from django.views.generic import View, UpdateView
 from .forms import PostCreateForm
 from .models import Post
 
@@ -32,11 +32,17 @@ class BlogCreateView(View):
         context={}
         return render(request, 'blog_create.html', context)
 
-
 class BlogDetailView(View):
+
     def get(self, request, pk, *args,**kwargs):
         post = get_object_or_404(Post, pk=pk)
         context={
             'post':post
         }
         return render(request, 'blog_detail.html', context)
+
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    fields=['title', 'content']
+    template_name='blog_update.html'
